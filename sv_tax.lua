9f4a8e8b8d914c2f36f3f3be6fdd8b032effc6c8
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-05-02 20:24:13 +0530

    fix ? & nil sql errors

--- sv_tax.lua
+++ sv_tax.lua
@@ -5,80 +5,82 @@ function PlayersTax()
     for i=1, #Players, 1 do
         local Player = QBCore.Functions.GetPlayer(Players[i])
         if Player then
-            local Amount = 0
-            if Player.PlayerData.job.name ~= 'police' and Player.PlayerData.job.name ~= 'ambulance' then
-                if Player.PlayerData.money.bank > Shared.EconomyTax['low'] or Player.PlayerData.money.cash > Shared.EconomyTax['low'] then  
-                    Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['low']/1000)                          
-                    TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['low'].."% by the government for $"..Amount..".")  
-                elseif Player.PlayerData.money.bank > Shared.EconomyTax['medium'] or Player.PlayerData.money.cash > Shared.EconomyTax['medium'] then                
-                    Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['medium']/1000)
-                    TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['medium'].."% by the government for $"..Amount..".")  
-                elseif Player.PlayerData.money.bank > Shared.EconomyTax['high'] or Player.PlayerData.money.cash > Shared.EconomyTax['high'] then                
-                    Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['high']/1000)
-                    TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['high'].."% by the government for $"..Amount..".")  
-                elseif Player.PlayerData.money.bank > Shared.EconomyTax['ultra'] or Player.PlayerData.money.cash > Shared.EconomyTax['ultra'] then                
-                    Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['ultra']/1000)
-                    TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['ultra'].."% by the government for $"..Amount..".")  
-                elseif Player.PlayerData.money.bank > Shared.EconomyTax['extreme'] or Player.PlayerData.money.cash > Shared.EconomyTax['extreme'] then                
-                    Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['extreme']/1000)
-                    TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['extreme'].."% by the government for $"..Amount..".")                
-                else
-                    Amount = 100
-                    TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at standard rate by the government for $100.")                                            
-                end
-                Player.Functions.RemoveMoney("bank", math.floor(Amount), "incometax")
+            local Amount = 0            
+            if Player.PlayerData.money.bank > Shared.EconomyTax['low'] or Player.PlayerData.money.cash > Shared.EconomyTax['low'] then  
+                Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['low']/1000)                          
+                TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['low'].."% by the government for $"..Amount..".")  
+            elseif Player.PlayerData.money.bank > Shared.EconomyTax['medium'] or Player.PlayerData.money.cash > Shared.EconomyTax['medium'] then                
+                Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['medium']/1000)
+                TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['medium'].."% by the government for $"..Amount..".")  
+            elseif Player.PlayerData.money.bank > Shared.EconomyTax['high'] or Player.PlayerData.money.cash > Shared.EconomyTax['high'] then                
+                Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['high']/1000)
+                TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['high'].."% by the government for $"..Amount..".")  
+            elseif Player.PlayerData.money.bank > Shared.EconomyTax['ultra'] or Player.PlayerData.money.cash > Shared.EconomyTax['ultra'] then                
+                Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['ultra']/1000)
+                TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['ultra'].."% by the government for $"..Amount..".")  
+            elseif Player.PlayerData.money.bank > Shared.EconomyTax['extreme'] or Player.PlayerData.money.cash > Shared.EconomyTax['extreme'] then                
+                Amount = (Player.PlayerData.money.bank*Shared.EconomyTaxPercentage['extreme']/1000)
+                TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at "..Shared.EconomyTaxPercentage['extreme'].."% by the government for $"..Amount..".")                
             else
-                TriggerClientEvent("QBCore:Notify", Players[i], "You have not been taxed as you work for government entity.")                                                        
+                Amount = 100
+                TriggerClientEvent("QBCore:Notify", Players[i], "You have been taxed at standard rate by the government for $100.")                                            
             end
+            Player.Functions.RemoveMoney("bank", math.floor(Amount), "incometax")
         end
     end
-    SetTimeout(Shared.EconomyTaxInterval * (60 * 1000), PlayersTax) -- 60 minutes
+    SetTimeout(Shared.EconomyTaxInterval * (60 * 1000), PlayersTax)
 end
 
 function CarsTax()     
     local Players = QBCore.Functions.GetPlayers()
     MySQL.Async.fetchAll('SELECT * FROM player_vehicles',{},function(Vehicles)        
         for i=1, #Players, 1 do 
-            local VehCount = 0            
-            for a=1, #Vehicles,1 do 
-                if Players[i].PlayerData.citizenid == Vehicles[a].citizenid and (Players[i].PlayerData.job.name ~= 'police' and Players[i].PlayerData.job.name ~= 'ambulance' and Players[i].PlayerData.job.name ~= 'realestate') then
-                    VehCount = VehCount + 1
+            local VehCount = 0  
+            local LP = QBCore.Functions.GetPlayer(Players[i])
+            if LP then          
+                for a=1, #Vehicles,1 do 
+                    if LP.PlayerData.citizenid == Vehicles[a].citizenid then
+                        VehCount = VehCount + 1
+                    end
                 end
-            end
-            if VehCount > 0 then
-                local VehTax = VehCount * Shared.CarTaxRate
-                local Player = QBCore.Functions.GetPlayerByCitizenId(Players[i].citizenid)
-                if Player then          
-                    Player.Functions.RemoveMoney("bank", math.floor(VehTax), "vehicletax")
-                    TriggerClientEvent("QBCore:Notify", Player.PlayerData.source, "You have been taxed $"..math.floor(VehTax).." as Vehicle Tax.")                               
-                end
-            end            
+                if VehCount > 0 then
+                    local VehTax = VehCount * Shared.CarTaxRate
+                    local Player = QBCore.Functions.GetPlayerByCitizenId(LP.PlayerData.citizenid)
+                    if Player then          
+                        Player.Functions.RemoveMoney("bank", math.floor(VehTax), "vehicletax")
+                        TriggerClientEvent("QBCore:Notify", Player.PlayerData.source, "You have been taxed $"..math.floor(VehTax).." as Vehicle Tax.")                               
+                    end
+                end   
+            end         
         end
     end)
-    SetTimeout(Shared.CarTaxInterval * (60 * 1000), CarsTax) -- 60 minutes
+    SetTimeout(Shared.CarTaxInterval * (60 * 1000), CarsTax)
 end
 
 function HousesTax()     
     local Players = QBCore.Functions.GetPlayers()
     MySQL.Async.fetchAll('SELECT * FROM player_houses',{},function(Houses)        
         for i=1, #Players, 1 do 
-            local HouseCount = 0            
-            for a=1, #Houses,1 do 
-                if Players[i].PlayerData.citizenid == Houses[a].citizenid and (Players[i].PlayerData.job.name ~= 'police' and Players[i].PlayerData.job.name ~= 'ambulance' and Players[i].PlayerData.job.name ~= 'realestate') then
-                    HouseCount = HouseCount + 1
+            local HouseCount = 0  
+            local LP = QBCore.Functions.GetPlayer(Players[i])
+            if LP then            
+                for a=1, #Houses,1 do 
+                    if LP.PlayerData.citizenid == Houses[a].citizenid then
+                        HouseCount = HouseCount + 1
+                    end
                 end
-            end
-            if HouseCount > 0 then
-                local HouseTax = HouseCount * Shared.HouseTaxRate
-                local Player = QBCore.Functions.GetPlayerByCitizenId(Players[i].citizenid)
-                if Player then          
-                    Player.Functions.RemoveMoney("bank", math.floor(HouseTax), "housetax")
-                    TriggerClientEvent("QBCore:Notify", Player.PlayerData.source, "You have been taxed $"..math.floor(HouseTax).." as House Tax.")                               
-                end
-            end            
+                if HouseCount > 0 then
+                    local HouseTax = HouseCount * Shared.HouseTaxRate
+                    local Player = QBCore.Functions.GetPlayerByCitizenId(LP.PlayerData.citizenid)
+                    if Player then          
+                        Player.Functions.RemoveMoney("bank", math.floor(HouseTax), "housetax")
+                        TriggerClientEvent("QBCore:Notify", Player.PlayerData.source, "You have been taxed $"..math.floor(HouseTax).." as House Tax.")                               
+                    end
+                end 
+            end           
         end
     end)
-    SetTimeout(Shared.HouseTaxInterval * (60 * 1000), CarsTax) -- 60 minutes
+    SetTimeout(Shared.HouseTaxInterval * (60 * 1000), HousesTax)
 end
 
 function GetCurrentTax(src, taxtype)
