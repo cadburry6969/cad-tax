aae9f63fd40ca229fdd519d59a9c21cee1e341ba
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-06-27 21:54:13 +0530

    Update shared.lua

--- shared.lua
+++ shared.lua
@@ -1,25 +1,25 @@
 Shared = {}
 
 Shared.EconomyTax = {
-    ['low'] = 500000,
-    ['medium'] = 1000000,
-    ['high'] = 5000000,    
-    ['ultra'] = 10000000,
-    ['extreme'] = 100000000,
+    ['low'] = 100000,
+    ['medium'] = 250000,
+    ['high'] = 500000,    
+    ['ultra'] = 1000000,
+    ['extreme'] = 2000000,
 }
 
 Shared.EconomyTaxPercentage = {
-    ['low'] = 1,
-    ['medium'] = 2,
-    ['high'] = 2,    
-    ['ultra'] = 2,
+    ['low'] = 0.1,
+    ['medium'] = 0.5,
+    ['high'] = 0.6,    
+    ['ultra'] = 0.8,
     ['extreme'] = 1,
 }
 
-Shared.EconomyTaxInterval = 60 -- in minutes
+Shared.EconomyTaxInterval = 120 -- in minutes (2 hrs)
 
-Shared.CarTaxRate = 250 -- $250 per car
-Shared.CarTaxInterval = 60 -- in minutes
+Shared.CarTaxRate = 100 -- $100 per car
+Shared.CarTaxInterval = 180 -- in minutes (3hrs)
 
-Shared.HouseTaxRate = 1000 -- $1000 per house
-Shared.HouseTaxInterval = 60 -- in minutes
+Shared.HouseTaxRate = 500 -- $500 per house 
+Shared.HouseTaxInterval = 180 -- in minutes (3hrs)
