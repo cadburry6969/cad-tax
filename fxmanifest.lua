7480a82800326f39df25ff3e2eae1947b0471618
cadburry <72443203+cadburry6969@users.noreply.github.com>
2022-06-27 21:47:47 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,10 @@ description "QBCore Tax System by Cadburry#7547"
 version "0.1.0"
 
 shared_script "shared.lua"
-server_script "sv_tax.lua"
+server_scripts {
+    "@oxmysql/lib/MySQL.lua",
+    "sv_tax.lua"
+}
 
 server_exports {
     "GetCurrentTax",    
